d99b327524ff49b988f4a492d6bb32426a7562cf
keiito <method03110201@gmail.com>
2021-04-25 10:24:53 +0900

    CSRF編集0425

--- www/html/login_process.php
+++ www/html/login_process.php
@@ -17,14 +17,17 @@ $token=get_post('token');
 
 $user = login_as($db, $name, $password);
 
-    if( $user === false){
-      set_error('ログインに失敗しました。');
-      redirect_to(LOGIN_URL);
-    }
-
+if(is_valid_csrf_token($token)){
+  if( $user === false){
+    set_error('ログインに失敗しました。');
+    redirect_to(LOGIN_URL);
+  }
   set_message('ログインしました。');
   if ($user['type'] === USER_TYPE_ADMIN){
     redirect_to(ADMIN_URL);
   }
-
+}else{
+  set_error('不正なログインです。');
+  redirect_to(LOGIN_URL);
+}
 redirect_to(HOME_URL);
--- www/html/signup_process.php
+++ www/html/signup_process.php
@@ -16,17 +16,23 @@ $password_confirmation = get_post('password_confirmation');
 $db = get_db_connect();
 $token=get_post('token');
 
-try{
-  $result = regist_user($db, $name, $password, $password_confirmation);
-  if( $result=== false){
+if(is_valid_csrf_token($token)){
+
+  try{
+    $result = regist_user($db, $name, $password, $password_confirmation);
+    if( $result=== false){
+      set_error('ユーザー登録に失敗しました。');
+      redirect_to(SIGNUP_URL);
+    }
+  }catch(PDOException $e){
     set_error('ユーザー登録に失敗しました。');
     redirect_to(SIGNUP_URL);
   }
-}catch(PDOException $e){
-  set_error('ユーザー登録に失敗しました。');
+  set_message('ユーザー登録が完了しました。');
+  login_as($db, $name, $password);
+
+}else{
+  set_error('不正な登録です。');
   redirect_to(SIGNUP_URL);
 }
-
-set_message('ユーザー登録が完了しました。');
-login_as($db, $name, $password);
 redirect_to(HOME_URL);
