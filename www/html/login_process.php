eabc2615da92daef0bd0fd4e04950d7a94af25b4
keiito <method03110201@gmail.com>
2021-04-26 21:27:11 +0900

    csrf編集0426

--- www/html/login_process.php
+++ www/html/login_process.php
@@ -15,17 +15,22 @@ $password = get_post('password');
 $db = get_db_connect();
 $token=get_post('token');
 
-$user = login_as($db, $name, $password);
 
 if(is_valid_csrf_token($token)){
-  if( $user === false){
+  try{
+    $user = login_as($db, $name, $password);
+    if( $user === false){
+      set_error('ログインに失敗しました。');
+      redirect_to(LOGIN_URL);
+    }
+  }catch(PDOException $e){
     set_error('ログインに失敗しました。');
     redirect_to(LOGIN_URL);
   }
-  set_message('ログインしました。');
-  if ($user['type'] === USER_TYPE_ADMIN){
-    redirect_to(ADMIN_URL);
-  }
+    set_message('ログインしました。');
+    if ($user['type'] === USER_TYPE_ADMIN){
+      redirect_to(ADMIN_URL);
+    }
 }else{
   set_error('不正なログインです。');
   redirect_to(LOGIN_URL);
